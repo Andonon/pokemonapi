2a61fc2910fc376b50b04b68d02f07327da3f56b
Troy Center <troycenter1@gmail.com>
2017-06-04 16:34:25 -0700

    Fixed the URL's and added the pokeid to the dom html so I can reference the idnumber

--- js/pokemon.js
+++ js/pokemon.js
@@ -8,7 +8,7 @@
         function getPokes(){
             // Get the pokemon by adding HTML to the div pokearea.
             for(var pokeid = 1; pokeid <= 151; pokeid++){
-                $("#pokearea").append("<img id=pokeID'"+pokeid+"' src='http://pokeapi.co/media/img/"+pokeid+".png' alt='Pokemon # "+pokeid+" from pokeapi.com'>");
+                $("#pokearea").append("<img id=pokeID"+pokeid+" pokeID='"+pokeid+"' src='http://pokeapi.co/media/img/"+pokeid+".png' alt='Pokemon # "+pokeid+" from pokeapi.com'>");
             }
         }
 
