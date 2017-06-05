3f94e8dc2834616cdf5481a148f828e0db7e6afc
Troy Center <troycenter1@gmail.com>
2017-06-04 19:56:09 -0700

    finsihed working on this. Good practice, has a bug. I need to make it cleanup with each new pokemon that is loaded.

--- js/pokemon.js
+++ js/pokemon.js
@@ -7,13 +7,28 @@
 
     }); 
 
-// get pokemon function 
+// get pokemon function and load first pokemon
         function getPokes(){
             // Get the pokemon by adding HTML to the div pokearea.
             for(var pokeid = 1; pokeid <= 151; pokeid++){
                 $("#pokearea").append("<img id=pokeID"+pokeid+" pokeID='"+pokeid+"' src='http://pokeapi.co/media/img/"+pokeid+".png' alt='Pokemon # "+pokeid+" from pokeapi.com'>");
             }
-        }
+                var thispokeid = 1;
+                $.get("http://pokeapi.co/api/v1/pokemon/"+thispokeid+"/", function(pokeinfo) {
+                var thispokename = pokeinfo.name;
+                $("#pokename").append("<h2>"+pokeinfo.name+"</h2>");
+                $("#mainpokeimage").attr("src","http://pokeapi.co/media/img/"+thispokeid+".png");
+                for(var i = 0; i < pokeinfo.types.length; i++){
+                    $("#poketypes").append("<li>"+pokeinfo.types[i].name+"</li>");
+                    };
+                $("#height").append("<p>"+pokeinfo.height+"</p>");
+                $("#weight").append("<p>"+pokeinfo.weight+"</p>");
+                for(var i = 0; i < pokeinfo.abilities.length; i++){
+                    $("#abilities").append("<p>"+pokeinfo.abilities[i].name+"</p>");
+                    };
+                $("#attackdefense").append("<p>"+pokeinfo.attack+" | "+pokeinfo.defense+"</p>");
+                }, "json");                
+            };
 
 // on clicking one of the pokemon, update the DOM html to show the info on the pokemon in the red box.  
 
@@ -30,6 +45,7 @@
 // use the API info to populate the information box. 
 
             var thispokename = pokeinfo.name;
+            $("h2").remove;    // why is this line not working? AHHHHHHHHHHHH.
             $("#pokename").append("<h2>"+pokeinfo.name+"</h2>");
             $("#mainpokeimage").attr("src","http://pokeapi.co/media/img/"+thispokeid+".png");
             for(var i = 0; i < pokeinfo.types.length; i++){
