9cfa599dd564ba1960bddfb54e1da5ca6eb9bdb2
Troy Center <troycenter1@gmail.com>
2017-06-04 18:52:52 -0700

    Finished the types api... moving on

--- js/pokemon.js
+++ js/pokemon.js
@@ -13,12 +13,14 @@
         }
 
         $(document).on("click", "img", function(){
+            
             var selectedpokemon = $(this).attr("pokeid");
             $.get("http://pokeapi.co/api/v1/pokemon/"+selectedpokemon+"/", function(pokeinfo) {
                 
                 ////////////////////////////////
                 //  console log testing area  //
                 ////////////////////////////////
+
                 console.log("Data Dump of API return:");
                 console.log(pokeinfo);
 
@@ -26,28 +28,38 @@
                 //  get the types             //
                 ////////////////////////////////
 
-
                 console.log("Types Name from Array:");
                 for(var i = 0; i < pokeinfo.types.length; i++){
                     console.log(pokeinfo.types[i].name);
-                    
-            }
-
+                    $("#poketypes").append("<li>"+pokeinfo.types[i].name+"</li>");
+                }
                 
+
+                ////////////////////////////////
+                //  get the Height            //
+                ////////////////////////////////
+                
+                console.log("Pokemon Height:");
+                console.log(pokeinfo.height);
+                $("#height").append("<p>"+pokeinfo.height+"</p>");
+
+
                 ////////////////////////////////
                 //  get the weight            //
                 ////////////////////////////////
                 
                 console.log("Pokemon Weigh:");
                 console.log(pokeinfo.weight);
-                
+                $("#weight").append("<p>"+pokeinfo.weight+"</p>");
+
                 ////////////////////////////////
                 //  get the abilities         //
                 ////////////////////////////////
                 
-                console.log("Abilities:")
+                console.log("Abilities:");
                 for(var i = 0; i < pokeinfo.abilities.length; i++){
                     console.log(pokeinfo.abilities[i].name);
+                    $("#abilities").append("<p>"+pokeinfo.abilities[i].name+"</p>");
                 }
 
                 ////////////////////////////////
@@ -57,7 +69,7 @@
                 console.log("Attack & Defense Numbers:");
                 console.log("Attack: "+pokeinfo.attack);
                 console.log("Defense: "+pokeinfo.defense);
-
+                $("#attackdefense").append("<p>"+pokeinfo.attack+" | "+pokeinfo.defense+"</p>");
 
 }, "json");            
         
